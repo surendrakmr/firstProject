98f5274e28c7bf38c5fecdc5d2febfda8d7bfb26
surendrakmr <surendrakumr@yahoo.com>
2018-10-23 10:32:29 +0530

    added

--- Tutorial/src/Test.java
+++ Tutorial/src/Test.java
@@ -4,8 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Just now added Hello world");
-		System.out.println("Third now added Hello world");
-		System.out.println("fourth now added Hello world");
+		
 	}
 
 }
